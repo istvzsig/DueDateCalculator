28638f637eb0d2ab3fc2d9829069bbf3e6c23916
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-10 16:25:11 +0200

    update constant name and update test decriptions

--- test/calculateTotalHours.test.js
+++ test/calculateTotalHours.test.js
@@ -1,12 +1,12 @@
 import DueDateCalculator from "../DueDateCalculator";
 
-describe("calculateTotalHours", () => {
-  const calculator = new DueDateCalculator();
+describe("Calculate Total Hours", () => {
+  const dueDateCalculator = new DueDateCalculator();
 
-  test("calculates total hours", () => {
+  test('It calculates hours from turnaround time.', () => {
     // Example: 2 days and 3 hours should result in 19 hours (2 * 8 + 3)
-    const turnaroundTime = 2 * calculator.workingHoursPerDay + 3;
-    const totalHours = calculator.calculateTotalHours(turnaroundTime);
-    expect(totalHours).toBe(19);
+    const turnaroundTime = 2 * dueDateCalculator.workingHoursPerDay + 3;
+    const totalHours = dueDateCalculator.calculateTotalHours(turnaroundTime);
+    expect(totalHours).toEqual(19);
   });
 });
