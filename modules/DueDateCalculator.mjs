7281fc0870ed78c3b428b6f5d789a06eb9fb64ef
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-10 15:35:36 +0200

    add properties to DueDateCalculator class

--- modules/DueDateCalculator.mjs
+++ modules/DueDateCalculator.mjs
@@ -1,4 +1,8 @@
 export default class DueDateCalculator {
   constructor() {
+    this.workingHoursPerDay = 8;
+    this.workingDaysPerWeek = 5;
+    this.workStartHour = 9;
+    this.workEndHour = 17;
   }
 }
