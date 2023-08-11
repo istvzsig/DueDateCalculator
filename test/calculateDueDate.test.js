2f453e93d7115210678c88619d95c9dc975cfb2d
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-11 15:31:09 +0200

    add test for calculateDueDate method covering 2 outcomes

---
+++ test/calculateDueDate.test.js
@@ -0,0 +1,21 @@
+import DueDateCalculator from "../DueDateCalculator";
+
+describe("calculateDueDate", () => {
+  const calculator = new DueDateCalculator();
+
+  test("calculates due date correctly within the same day", () => {
+    const submitDate = new Date("2023-08-16T14:00:00");
+    const turnaroundTime = 3;
+    const dueDate = calculator.calculateDueDate(submitDate, turnaroundTime);
+    const expectedDueDate = new Date("2023-08-16T17:00:00");
+    expect(dueDate).toEqual(expectedDueDate);
+  });
+
+  test("calculates due date correctly across multiple days", () => {
+    const submitDate = new Date("2023-08-14T14:12:00");
+    const turnaroundTime = 16;
+    const dueDate = calculator.calculateDueDate(submitDate, turnaroundTime);
+    const expectedDueDate = new Date("2023-08-16T14:12:00");
+    expect(dueDate).toEqual(expectedDueDate);
+  });
+});
