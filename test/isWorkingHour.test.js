a6ac5cbb7a2401198c6bf36a6f79ead0573280be
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-11 16:49:48 +0200

    + tests for isWorkingHour

--- test/isWorkingHour.test.js
+++ test/isWorkingHour.test.js
@@ -4,19 +4,31 @@ describe("isWorkingHour", () => {
   const calculator = new DueDateCalculator();
 
   test("Returns true if time is within working hours", () => {
-    // Wednesday, 2:00 PM
+    // Wednesday, 2:00
     const date = new Date("2023-08-16T14:00:00");
     expect(calculator.isWorkingHour(date)).toBe(true);
   });
 
+  test("Returns true if time is within working hours", () => {
+    // Monday, 14:41:55
+    const date = new Date("2023-08-14T14:41:55");
+    expect(calculator.isWorkingHour(date)).toBe(true);
+  });
+
   test("Returns false if time is before working hours", () => {
-    // Thursday, 7:00 AM
+    // Thursday, 7:00
     const date = new Date("2023-08-17T07:00:00");
     expect(calculator.isWorkingHour(date)).toBe(false);
   });
 
   test("Returns false if time is after working hours", () => {
-    // Friday, 6:00 PM
+    // Friday, 18:41:55
+    const date = new Date("2023-08-18T18:41:55");
+    expect(calculator.isWorkingHour(date)).toBe(false);
+  });
+  
+  test("Returns false if time is within working hours and not working day", () => {
+    // Sunday, 14:47
     const date = new Date("2023-08-18T18:00:00");
     expect(calculator.isWorkingHour(date)).toBe(false);
   });
