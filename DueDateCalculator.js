8cee44557fac7d6947041b5db356696aee43c913
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-11 17:37:05 +0200

    refactor and create global constants

--- DueDateCalculator.js
+++ DueDateCalculator.js
@@ -1,9 +1,19 @@
-export default class DueDateCalculator {
+import {
+  INCREMENT,
+  WORKING_HOURS_PER_DAY,
+  WORKING_DAYS_PER_WEEK,
+  WORK_START_HOUR,
+  WORK_END_HOUR,
+  FIRST_WORK_DAY,
+} from './constants/dueDateCalculator.mjs';
+
+class DueDateCalculator {
   constructor() {
-    this.workingHoursPerDay = 8;
-    this.workingDaysPerWeek = 5;
-    this.workStartHour = 9;
-    this.workEndHour = 17;
+    this.workingHoursPerDay = WORKING_HOURS_PER_DAY;
+    this.workingDaysPerWeek = WORKING_DAYS_PER_WEEK;
+    this.workStartHour = WORK_START_HOUR;
+    this.workEndHour = WORK_END_HOUR;
+    this.firstWorkDay = FIRST_WORK_DAY;
   }
 
   calculateDueDate(submitDate, turnaroundTime) {
@@ -17,14 +27,15 @@ export default class DueDateCalculator {
     let remainingHours = totalHours;
 
     while (remainingHours > 0) {
-      dueDate.setHours(dueDate.getHours() + 1);
+      dueDate.setHours(dueDate.getHours() + INCREMENT);
       if (this.isWorkingDay(dueDate) && this.isWorkingHour(dueDate)) {
         remainingHours--;
       }
       if (!this.isWorkingHour(dueDate)) {
         // Move to the start of the next working day
         dueDate.setHours(this.workStartHour);
-        dueDate.setDate(dueDate.getDate() + 1); // Increment the date here
+        // Increment the date here
+        dueDate.setDate(dueDate.getDate() + INCREMENT);
       }
     }
     return dueDate;
@@ -38,7 +49,7 @@ export default class DueDateCalculator {
 
   isWorkingDay(date) {
     const dayOfWeek = date.getDay();
-    return dayOfWeek >= 1 && dayOfWeek <= this.workingDaysPerWeek && this.isWorkingHour(date);
+    return dayOfWeek >= this.firstWorkDay && dayOfWeek <= this.workingDaysPerWeek && this.isWorkingHour(date);
   }
 
   isWorkingHour(date) {
@@ -46,3 +57,5 @@ export default class DueDateCalculator {
     return hour >= this.workStartHour && hour <= this.workEndHour;
   }
 }
+
+export default DueDateCalculator;
