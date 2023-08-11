3236d291c1d959ddb0b38d1e2d827bd426dcbd55
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-11 19:32:54 +0200

    rename folder constants -> constant

---
+++ constant/dueDateCalculatorConstants.mjs
@@ -0,0 +1,6 @@
+export const INCREMENT = 1;
+export const WORKING_HOURS_PER_DAY = 8;
+export const WORKING_DAYS_PER_WEEK = 5;
+export const WORK_START_HOUR = 9;
+export const WORK_END_HOUR = 17;
+export const FIRST_WORK_DAY = 1;
