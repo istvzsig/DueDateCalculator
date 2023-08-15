fddc0759ea45644dba1471477548cd0f05620c16
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-15 08:55:48 +0200

    fix typo in dateModifier test and update import path

--- test/dateModifier.test.js
+++ test/dateModifier.test.js
@@ -1,5 +1,5 @@
 import DateModifier from "../modifier/DateModifier.mjs";
-import { INCREMENT_BY_1 } from "../constant/dueDateCalculatorConstants.mjs";
+import { INCREMENT_BY_1 } from "../constant/math.mjs";
 
 describe("Modify Date object", () => {
   const dateModifier = DateModifier;
@@ -18,27 +18,27 @@ describe("Modify Date object", () => {
     expect(modifiedHour + INCREMENT_BY_1).toBe(expectedHour);
   });
 
-  test("Returns true if submit date can be parsed", () => {
+  test("Returns true if date can be parsed", () => {
     const date = new Date("2023-08-14T14:12:00");
     expect(dateModifier.isValidDate(date)).toBe(true);
   });
 
-  test("Returns true if submit date can be parsed", () => {
+  test("Returns true if date can be parsed", () => {
     const text = "2023-08-14T14:12:00";
     expect(dateModifier.isValidDate(text)).toBe(true);
   });
 
-  test("Returns true if submit date can be parsed", () => {
+  test("Returns true if date can be parsed", () => {
     const text = "2023-08-14";
     expect(dateModifier.isValidDate(text)).toBe(true);
   });
 
-  test("Returns false if submit date can not be parsed", () => {
+  test("Returns false if date can not be parsed", () => {
     const text = "55fsad323DDK__asd2";
     expect(dateModifier.isValidDate(text)).toBe(false);
   });
   
-  test("Returns false if submit date can not be parsed", () => {
+  test("Returns false if date can not be parsed", () => {
     const text =  "2023->>??08-14XXX";
     expect(dateModifier.isValidDate(text)).toBe(false);
   });
