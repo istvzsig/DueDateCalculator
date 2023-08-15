fb8f27643f54b24f6922aaab7a296e689d0b5d83
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-15 09:54:06 +0200

    add test and fix typo in calculateDueDateFromTotalHours test

--- test/calculateDueDateFromTotalHours.test.js
+++ test/calculateDueDateFromTotalHours.test.js
@@ -1,13 +1,29 @@
 import DueDateCalculator from "../DueDateCalculator";
 
-describe("Is working hour", () => {
+describe("Calculates due date from total hours", () => {
     const calculator = new DueDateCalculator();
 
-    test('calculates due date correctly across multiple days', () => {
-        const submitDate = new Date('2023-08-14T14:12:00');
-        const totalHours = 16;
-        const expectedDueDate = new Date('2023-08-16T14:12:00');
-        const dueDate = calculator.calculateDueDateFromTotalHours(submitDate, totalHours);
+    test("Calculates due date correctly within the same day", () => {
+        const submitDate = new Date("2023-08-16T14:00:00");
+        const turnaroundTime = 3;
+        const dueDate = calculator.calculateDueDate(submitDate, turnaroundTime);
+        const expectedDueDate = new Date("2023-08-16T17:00:00");
+        expect(dueDate).toEqual(expectedDueDate);
+      });
+    
+      test("Calculates due date correctly across multiple days", () => {
+        const submitDate = new Date("2023-08-14T14:12:00");
+        const turnaroundTime = 16;
+        const dueDate = calculator.calculateDueDate(submitDate, turnaroundTime);
+        const expectedDueDate = new Date("2023-08-16T14:12:00");
+        expect(dueDate).toEqual(expectedDueDate);
+      });
+
+    test('Calculates due date correctly across multiple days', () => {
+        const submitDate = new Date("2023-08-16T14:12:00");
+        const turnaroundTime = 24;
+        const dueDate = calculator.calculateDueDate(submitDate, turnaroundTime);
+        const expectedDueDate = new Date("2023-08-21T14:12:00");
         expect(dueDate).toEqual(expectedDueDate);
     });
 });
