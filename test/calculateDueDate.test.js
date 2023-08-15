7ca664bbaab92f3fee49440d7d4df5c44e283ff2
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-15 09:02:36 +0200

    add math constant and fix typo in calculateDueDate test

--- test/calculateDueDate.test.js
+++ test/calculateDueDate.test.js
@@ -1,9 +1,9 @@
 import DueDateCalculator from "../DueDateCalculator";
 
-describe("calculateDueDate", () => {
+describe("Calculate due date", () => {
   const calculator = new DueDateCalculator();
 
-  test("calculates due date correctly within the same day", () => {
+  test("Calculates due date correctly within the same day", () => {
     const submitDate = new Date("2023-08-16T14:00:00");
     const turnaroundTime = 3;
     const dueDate = calculator.calculateDueDate(submitDate, turnaroundTime);
@@ -11,7 +11,7 @@ describe("calculateDueDate", () => {
     expect(dueDate).toEqual(expectedDueDate);
   });
 
-  test("calculates due date correctly across multiple days", () => {
+  test("Calculates due date correctly across multiple days", () => {
     const submitDate = new Date("2023-08-14T14:12:00");
     const turnaroundTime = 16;
     const dueDate = calculator.calculateDueDate(submitDate, turnaroundTime);
@@ -19,7 +19,7 @@ describe("calculateDueDate", () => {
     expect(dueDate).toEqual(expectedDueDate);
   });
 
-  test("calculates due date correctly across multiple days including not working days", () => {
+  test("Calculates due date correctly across multiple days including not working days", () => {
     const submitDate = new Date("2023-08-16T14:12:00");
     const turnaroundTime = 24;
     const dueDate = calculator.calculateDueDate(submitDate, turnaroundTime);
