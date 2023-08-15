f3dd8f9506c05aa059289b9de03cd221216a07d7
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-15 08:53:48 +0200

    fix typo in isWorkingDay test

--- test/isWorkingDay.test.js
+++ test/isWorkingDay.test.js
@@ -1,27 +1,27 @@
 import DueDateCalculator from "../DueDateCalculator";
 
-describe("Submitted on work day", () => {
+describe("Is working day", () => {
   const calculator = new DueDateCalculator();
 
-  test("returns true for a weekday within working hours", () => {
+  test("Returns true for a weekday within working hours", () => {
     // Monday, 10:30 AM
     const date = new Date("2023-08-14T10:30:00");
     expect(calculator.isWorkingDay(date)).toBe(true);
   });
 
-  test("returns false for a weekend day", () => {
+  test("Returns false for a weekend day", () => {
     // Sunday, 2:00 PM
     const date = new Date("2023-08-13T14:00:00");
     expect(calculator.isWorkingDay(date)).toBe(false);
   });
 
-  test("returns false for a weekday before working hours", () => {
+  test("Returns false for a weekday before working hours", () => {
     // Tuesday, 7:00 AM
     const date = new Date("2023-08-15T07:00:00");
     expect(calculator.isWorkingDay(date)).toBe(false);
   });
 
-  test("returns false for a weekday after working hours", () => {
+  test("Returns false for a weekday after working hours", () => {
     // Wednesday, 6:00 PM
     const date = new Date("2023-08-16T18:00:00");
     expect(calculator.isWorkingDay(date)).toBe(false);
